b5751fab17acd5c4f00b1d3012d62263e249fb41
Nakiami <wjl8927962@vip.qq.com>
2019-03-08 13:51:02 +0800

    修改init文件加载语句错误

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,13 +16,13 @@ INIT=$HOME/.local/etc/init.sh
 
 if [ -f "$HOME/.bashrc" ]
 then
-    echo $INIT >> $HOME/.bashrc
+    echo "source $INIT" >> $HOME/.bashrc
     echo "umask 022" >> $HOME/.bashrc
 fi
 
 if [ -f "$HOME/.zshrc" ]
 then
-    echo $INIT >> $HOME/.zshrc
+    echo "source $INIT" >> $HOME/.zshrc
     echo "umask 022" >> $HOME/.zshrc
 fi
 
