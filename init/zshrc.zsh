f640078a81141ba2e4930df568514772a5fed801
Nakiami <wjl8927962@vip.qq.com>
2019-03-13 23:38:57 +0800

    add a new plugin z.lua to zsh config

--- init/zshrc.zsh
+++ init/zshrc.zsh
@@ -46,12 +46,16 @@ antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
 #antigen bundle git
-antigen bundle z
-antigen bundle command-not-found
+#antigen bundle z
+#antigen bundle command-not-found
 
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
+
+antigen bundle skywind3000/z.lua
+export _ZL_ADD_ONCE=1
+export _ZL_MATCH_MODE=1
 
 # Load the theme.
 antigen theme robbyrussell
