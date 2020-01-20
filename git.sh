1d97f5942f16d013751df498542e701198d22ab3
wNakiami <wjl8927962@vip.qq.com>
2020-01-21 01:33:16 +0800

    bugfixed

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-if [[ -n (command -v git) ]]; then
+if [[ -n $(command -v git) ]]; then
     git config --global pager.status false
     git config --global pager.branch false
     git config --global pager.stash false
