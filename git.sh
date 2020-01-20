768277ba4dfd871c3988e916d9d8ce53b1f7cc27
wNakiami <wjl8927962@vip.qq.com>
2020-01-21 01:55:04 +0800

    git config credential.helper

--- git.sh
+++ git.sh
@@ -8,6 +8,8 @@ if [[ -n $(command -v git) ]]; then
 
     git config --global icdiff.options '--line-numbers'
 
+    git config --global credential.helper store
+
     git config --global user.name 'wNakiami'
     git config --global user.email 'wjl8927962@vip.qq.com'
 fi
